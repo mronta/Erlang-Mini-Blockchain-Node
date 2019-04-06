9b1915ffae41ad8c8cc73c0822c0c627ff9a5673
DarthVi <covinc93@gmail.com>
2019-04-06 19:33:24 +0200

    Experiments on update block gossiping: not yet completed

--- cotaro_node.erl
+++ cotaro_node.erl
@@ -1,73 +1,162 @@
 -module(cotaro_node).
 -export([loop/2, test_nodes/0]).
 
+-define(NumberOfFriendsRequired, 3).
+
 sleep(N) -> receive after N*1000 -> ok end.
 
-%watcher che controlla periodicamente che il nodo sia vivo
+loop(MyFriends, State) ->
+    case MyFriends of
+        [] ->   io:format("~p require to teacher_node new friends ~n", [self()]),
+                global:send(teacher_node, {get_friends, self(), make_ref()});
+        _ ->    io:format("~p has at least one friend ~n", [self()])
+    end,
+    receive
+
+        {ping, Mittente, Nonce} ->  Mittente ! {pong, Nonce},
+                                    io:format("~p has received ping request, sending pong~n", [self()]),
+                                    loop(MyFriends, State) ;
+
+
+        {dead, Node} -> io:format("For ~p the node ~p is dead~n",[self(), Node]),
+                        MyFriendsUpdated = MyFriends -- [Node],
+                        askFriends(MyFriendsUpdated),
+                        loop(MyFriendsUpdated, State);
+
+
+        {friends, _, OtherFriends} ->   io:format("~p receive friend list ~n", [self()]),
+                                        NewFriends = OtherFriends -- (MyFriends ++ [self()]),
+                                        MyNewListOfFriends = addFriends(MyFriends, NewFriends),
+                                        loop(MyNewListOfFriends, State);
+
+
+        {get_friends, Sender, Nonce} -> io:format("~p send friend list to ~p ~n", [self(), Sender]),
+                                        Sender ! {friends, Nonce, MyFriends},
+                                        loop(MyFriends, State)
+
+    end.
+
+
+
+askFriends([]) ->   io:format("~p require friends to teacher node~n", [self()]),
+                    global:send(teacher_node, {get_friends, self(), make_ref()});
+askFriends(MyFriends) ->    io:format("~p require friends to another node~n", [self()]),
+                            Nonce = make_ref(),
+                            lists:nth(rand:uniform(length(MyFriends)), MyFriends) ! {get_friends, self(), Nonce}.
+
+
+addFriends(MyFriends, []) -> MyFriends;
+addFriends([], OtherFriends) ->
+    NewFriend = lists:nth(rand:uniform(length(OtherFriends)), OtherFriends),
+    io:format("~p add a new friend ~p~n",[self(), NewFriend]),
+    Self = self(),
+    spawn(fun () -> watch(Self,NewFriend) end),
+    addFriends([NewFriend], OtherFriends -- [NewFriend]);
+addFriends(MyFriends, OtherFriends) ->
+    case length(MyFriends) < ?NumberOfFriendsRequired of
+        true -> NewFriend = lists:nth(rand:uniform(length(OtherFriends)), OtherFriends),
+                io:format("~p add a new friend ~p~n",[self(), NewFriend]),
+                Self = self(),
+                spawn(fun () -> watch(Self,NewFriend) end),
+                addFriends( MyFriends ++ [NewFriend], OtherFriends -- [NewFriend]);
+        false -> io:format("~p friend list = ~w ~n", [self(), MyFriends]),
+                 MyFriends
+    end.
+
+
 watch(Main,Node) ->
   sleep(10),
   Ref = make_ref(),
   Node ! {ping, self(), Ref},
   receive
     {pong, Ref} -> watch(Main,Node)
-  after 10000 -> Main ! {dead, Node}
+  after 2000 -> Main ! {dead, Node}
   end.
 
+% può essere usata per ottenere il blocco richiesto da messaggio 'get_previous'
+getBlockFromChain(CurrentChain, BlockID) ->
+    {chain, _, CurrentDictChain} = CurrentChain,
+    {ok, Block} = dict:find(BlockID, CurrentDictChain),
+    Block.
 
-loop(Nodes, State) ->
-  receive
-		{ping, Watcher, WatcherRef} -> Watcher ! {pong, WatcherRef} ,
-							   io:format("~p has received ping request, sending pong~n", [self()])
-  after 2000 -> global:send(teacher_node, {get_friends, self(), make_ref()})
-  end,
-  receive
+% crea un dizionario sulla base di quello originale considerando solo la "catena"
+% che parte da BlockID
+iterDictChain(OriginalDict, Dict, BlockID) ->
+    %non dovrebbero essere possibili errori
+    Block = getBlockFromChain(OriginalDict, BlockID),
+    NewDict = dict:append(BlockID, Block, Dict),
+    {_, PreviousBlockID, _, _} = Block,
+    case PreviousBlockID of
+        none -> NewDict;
+        ExistingID -> iterDictChain(OriginalDict, NewDict, ExistingID)
+    end.
 
-	%{ping, Sender, Nonce} -> Sender ! {pong, Nonce};
+% Alla chiamata della funzione NewDictChain è un dizionario vuoto
+newDictChain(SenderPID, Nonce, Block, CurrentDictChain, NewDictChain) ->
+    {BlockID, PreviousBlockID, _, _} = Block,
+    NewDictChain = dict:append(BlockID, Block, NewDictChain),
+    case PreviousBlockID =:= none of
+        true -> NewDictChain;
+        false -> case dict:find(PreviousBlockID, CurrentDictChain) of
+            {ok, Value} ->
+                RemainingDict = iterDictChain(CurrentDictChain, dict:new(), PreviousBlockID),
+                dict:merge(fun(K, V1, V2) -> V1 end, NewDictChain, RemainingDict);
+            error ->
+                SenderPID ! {get_previous, SenderPID, Nonce, PreviousBlockID},
+                receive
+                    {previous, Nonce, PreviousBlock} ->
+                        newDictChain(
+                            SenderPID, 
+                            Nonce, 
+                            PreviousBlock, 
+                            CurrentDictChain,
+                            NewDictChain
+                        )
+                end
+        end
+    end.
 
-    {get_friends, Sender, Nonce} ->
-       Sender ! {friends, Nonce, Nodes},
-       loop(Nodes, State) ;
-	%%se uno dei nodi muore, manda a se stesso un messaggio "get_more_friends" e torna in loop
-    {dead, Node} ->
-       io:format("Dead node ~p~n",[Node]),
-	   Ref = make_ref(),
-	   self() ! {get_more_friends, Ref, Nodes -- [Node]},
-       loop(Nodes -- [Node], State);
+% CurrentChain {chain, HeadBlock, DictChain}
+newUpdateChain(SenderPID, Nonce, NewBlock, CurrentChain) ->
+    {NewBlockID, _, _, _} = NewBlock,
+    {chain, _, CurrentDictChain} = CurrentChain,
+    {
+        chain, 
+        NewBlockID,
+        newDictChain(SenderPID, Nonce, NewBlock, CurrentDictChain, dict:new())
+    }.
 
-	{get_more_friends, Ref, CurrentNodes} -> getMoreFriends(self(), Ref, CurrentNodes);
+% ritorna la catena più lunga tra le due in input
+% se la lunghezza è la stessa, viene ritornata la prima
+getLongestChain(Chain1, Chain2) ->
+    {chain, _, DictChain1} = Chain1,
+    {chain, _, DictChain2} = Chain2,
+    case length(dict:fetch_keys(DictChain1)) >= length(dict:fetch_keys(DictChain2)) of
+        true -> Chain1;
+        false -> Chain2
+    end.
 
-	{friends, Ref, AmiciAltrui} -> AmiciDiversi = AmiciAltrui -- [self()|Nodes], %elimino dalla lista ottenuta me stesso e gli amici che già ho
-								   Nuovo_Amico = case length(AmiciDiversi) > 0 of %se la lista ottenuta non è vuota, scelgo un amico a caso
-															
-													true -> io:format("~p: AmiciDiversi: ~p~n", [self(), AmiciDiversi]),
-															F = lists:nth(rand:uniform(length(AmiciDiversi)), AmiciDiversi),
-															io:format("L'attore ~p sta aggiungendo un amico ~p.~n", [self(), F]),
-															spawn(fun () -> watch(self(),F) end), %spawno il watcher per il nuovo amico
-															F;
+%Controllo se la soluzione per il proof of work sia corretta, in caso negativo la nostra chain resta immutata,
+%altrimenti faccio le seguenti operazioni: se abbiamo già il blocco nel nostro dizionario, non aggiungo nulla e non faccio gossiping,
+%altrimenti aggiungo il blocco nel dizionario e faccio gossiping dell'update
+updateBlock(Block, OurCurrentChain, Friends) -> {IDnuovo_blocco, ID_bloccoprecedente, Lista_transazioni, Soluzione} = Block,
+												{chain, Head, CurrChain} = OurCurrentChain,
+									  Chain = case proof_of_work:check(ID_bloccoprecedente, Lista_transazioni) of
+										   true -> case dict:find(IDnuovo_blocco, CurrChain) of
+													   {ok, Value} -> OurCurrentChain;
+													   error -> NewChain = dict:store(IDnuovo_blocco, Block, OurCurrentChain),
+																[F ! {update, Block} || F <- Friends],
+																{chain, Block, NewChain}
+												   end;   
+										   false -> OurCurrentChain 
+											   end,
+										   %cerchiamo sempre di determinare la catena più lunga fra la nostra e quella degli amici
+										   %in modo da risolvere i casi di fork o qualora ci siamo appena connessi alla rete
+									       getLongestChain(Chain, Friends).
 
-													false -> []
-								   end,
-								   loop([Nuovo_Amico|Nodes], State)
+getLongestChain(Chain, Friends) -> Chain. %TODO: cambiare questa funzione quando sarà disponibile il codice per ricostruire la catena
 
-    after 2000 ->
-	   case length(Nodes) < 3 of
-		   true -> Ref = make_ref(),
-				   self() ! {get_more_friends, Ref, Nodes},
-				   io:format("L'attore ~p sta mandando a se stesso una get_more_friends", [self()]);
-		   false -> ok
-	   end,
-	   loop(Nodes, State)
-	end.
-
-%% se abbiamo ancora amici, chiediamo la lista di amici ad un nostro amico, altrimenti la chiediamo al teacher_node
-getMoreFriends(PID, Nonce, Nodes) ->
-	io:format("L'attore ~p ha fatto chiamata a getMoreFriends.~n", [PID]),
-	case length(Nodes) > 0 of
-		true -> lists:nth(rand:uniform(length(Nodes)), Nodes) ! {get_friends, PID, Nonce};
-		false -> global:send(teacher_node, {get_friends, PID, Nonce})
-	end.
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 test_nodes() ->
 	T = spawn(teacher_node, main, []),
 	sleep(5),
@@ -76,7 +165,9 @@ test_nodes() ->
 	M3 = spawn(?MODULE, loop, [[],[]]),
 	M4 = spawn(?MODULE, loop, [[],[]]),
 	io:format("Finito di spawnare 4 nodi + teacher_node~n"),
-	sleep(2),
+    sleep(15),
+    exit(M2, manually_kill),
+	sleep(15),
 	Ref = make_ref(),
 	M1 ! {get_friends, self(), Ref},
 	receive
